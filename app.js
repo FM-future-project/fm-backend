c78026caf267be321e36c5f43a5cf16a43f6bcfc
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2022-02-14 12:24:47 +0200

    FF-27 set connection with local SQL Server database

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const sql = require('mssql');
+require('dotenv').config();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({ message: 'Добро пожаловать!!!' });
+});
+
+const config = {
+  user: process.env.DB_LOGIN,
+  password: process.env.DB_PWD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB,
+  options: {
+    trustServerCertificate: true,
+  },
+};
+
+app.get('/db', (req, res) => {
+  let connection = sql.connect(config, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send('DB Connected');
+    }
+  });
 });
 
 module.exports = app;
